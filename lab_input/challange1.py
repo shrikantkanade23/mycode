bf158d01e1bdcdece37967eafd5e46f1ca8b0159
Shrikant Kanade <shrikantkanade23@users.noreply.github.com>
2021-11-08 14:33:05 +0000

    learning about input function

--- lab_input/challange1.py
+++ lab_input/challange1.py
@@ -4,4 +4,4 @@ user_name = input("Please Enter User Name:")
 
 week = input("what day is today:")
 
-print("Hello" + user_name+"! Happy " + week + "!")
+print("Hello " + user_name+"! Happy " + week + "!")
