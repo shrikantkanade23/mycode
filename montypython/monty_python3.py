490a12b416ae2e3c1030aa33362cb74405d1c72c
Shrikant Kanade <shrikantkanade23@users.noreply.github.com>
2021-11-09 15:53:20 +0000

    While looping

--- montypython/monty_python3.py
+++ montypython/monty_python3.py
@@ -9,9 +9,7 @@ answer = " "
 # you could reduce the complexity of this conditional with some "break" statements
 while round < 3 and (answer != "Brian" and answer != "Shrubbery"):
     round += 1     # increase the round counter by 1
-    answer = input('Finish the movie title, "Monty Python\'s The Life of ______": ')
-    answer = answer.capitalize() # this line inserted to line 8 will make all
-                                 # user input starts with an uppercase
+    answer = input('Finish the movie title, "Monty Python\'s The Life of ______": ').strip().capitalize()
     if answer == "Brian": # logic to check if user gave correct answer
         print("Correct!")
     elif answer == "Shrubbery":
