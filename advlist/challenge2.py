f6b607aa764562d6f8c29b26b2412ba302dfca47
Shrikant Kanade <shrikantkanade23@users.noreply.github.com>
2021-11-09 10:36:02 +0000

    learning about dictionaries

---
+++ advlist/challenge2.py
@@ -0,0 +1,9 @@
+icecream= ["indentation", "    "] 
+northerntrust= ["Darrin","Hiraman","Marc","Michael","Prarthana","Shrikant"]
+
+icecream.append(4)
+print(icecream)
+num = input ("Please Enter any one number between 0 to 5 ")
+
+print ( % northerntrust[num])
+###+ " always uses " + icecream[-1] + icecream[1] + " to indent.")
